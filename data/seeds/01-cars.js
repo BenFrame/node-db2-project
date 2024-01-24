76ea8e4dd2f6e28b3bdeef68aa7846d0eb406aea
Ben Frame <brewerytattooer@gmail.com>
2024-01-24 13:52:51 -0500

    wip

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,7 +1,7 @@
 // STRETCH
 const cars = [
     {
-        vin: '1111111111111', 
+        vin: 'WBAET37404NJ42149', 
         make: 'toyota', 
         model: 'prius', 
         mileage: 215000, 
@@ -9,7 +9,7 @@ const cars = [
         transmission: 'manual',
     },
     {
-        vin: '2222222222222', 
+        vin: '2G4WD582391176443', 
         make: 'toyota', 
         model: 'corola', 
         mileage: 115000, 
@@ -17,7 +17,7 @@ const cars = [
         
     },
     {
-        vin: '3333333333333', 
+        vin: '1G1PC5SB2D7146892', 
         make: 'ford', 
         model: 'focus', 
         mileage: 15000, 
